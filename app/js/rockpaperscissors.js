7759eff9d53bbf5baf0af5e26e5f70af50a27cd1
Karolin Rafalski <kr2174@caa.columbia.edu>
2016-01-06 00:04:00 -0500

    completed game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -4,8 +4,7 @@
 'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    return prompt("Please choose either 'rock', 'paper', or 'scissors'. Type 'quit' to quit.");
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -25,31 +24,112 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
+
+function getWinner(winner) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+  
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    function showMoves(){
+        alert('You chose ' + playerMove + '. The computer chose ' + computerMove);
+    }
+    
+
+
+if (playerMove === computerMove){
+    showMoves();
+     winner = 'tie';
+     return winner;
+}
+
+if (playerMove ==='rock'){
+     showMoves();
+     if (computerMove === 'paper'){
+        
+        winner = 'computer';
+     }
+     else {
+        winner = 'player';
+     }
+     return winner;
+}
+
+if (playerMove ==='scissors'){
+     showMoves();
+     if (computerMove === 'rock'){
+        winner = 'computer';
+     }
+     else {
+        winner = 'player';
+     }
+     return winner;
+}
+
+if (playerMove ==='paper'){
+    showMoves();
+     if (computerMove === 'scissors'){
+        winner = 'computer';
+     }
+     else {
+        winner = 'player';
+     }
+     return winner;
+}
+
+if (playerMove ==="quit"){
+    winner = "quit";
     return winner;
 }
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
 }
 
+//when using playTo in console be sure to put a numeric value in parenthesis!
+function playTo(x) {
+  console.log("Let's play Rock, Paper, Scissors");
+  var playerWins = 0;
+  var computerWins = 0;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    var winCount = x;
+      do{  var winner = getWinner();
+            if (winner === 'player'){
+                playerWins = playerWins + 1 ;
+                alert("Player wins this round!" + "The score is "
+                + playerWins + " player : " +
+                computerWins + " computer");
+            } else if (winner ==='computer'){
+                computerWins = computerWins + 1 ;
+                   alert("Computer wins this round!"+ "The score is "
+                + playerWins + " player : " +
+                computerWins + " computer");
+            }else if (winner ==='tie'){
+                alert("It's a tie!" + " The score is "
+                + playerWins + " player : " +
+                computerWins + " computer");
+                /* winner = tie, add nothing, repeat*/
+            }else if (winner ==="quit"){
+                winCount = "quit";
+              break;  
+            } else {alert("Oops! There was an error! Please type carefully!");}
+        } while (playerWins < winCount && computerWins < winCount && winCount !=="quit")
+             alert("The final score is " + playerWins + " player: " + computerWins + " computer.")
+             if (playerWins>computerWins){
+                 alert("You are the champion!");
+             }else if (winCount ==="quit"){
+                 alert("Please play again some time!");
+             } else {alert("Computer is the champion!");}
+            return [playerWins, computerWins];
+}
+
+playTo(5);
